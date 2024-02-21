69805dad4fa5d32dd1af916564bd95cf79526148
lantern araru <106447625+lanternarasu@users.noreply.github.com>
2024-02-21 13:43:23 +0530

    Add files via upload

---
+++ frontend/loginIn_regiser/firstPage.js
@@ -0,0 +1,7 @@
+document.getElementById('loginButton').addEventListener('click',function(){
+    window.location.href = 'signIn.html';}
+    )
+    document.getElementById('signupButton').addEventListener('click',function() {
+        window.location.href = 'register.html';}
+        )
+    
