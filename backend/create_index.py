7baaa18dcd592273eb00ef23669436e185823b07
Sathya Krishnan <ts.sathyakrishnan2002@gmail.com>
2024-04-03 16:30:49 +0530

    updated index code

--- backend/create_index.py
+++ backend/create_index.py
@@ -9,17 +9,17 @@ from typing import List, Union
 
 
 # ------- CONSTANTS ------
-DIR_PATH = "../data/numpy"
+DIR_PATH = "./data/{library_name}"
 EMBEDDING_MODEL = "BAAI/bge-base-en-v1.5"
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 200
-INDEX_PATH = "./vector_stores/index"
+INDEX_PATH = "./vector_stores/{library_name}"
 # -----------------------
 
 
-def create_chunks() -> List[Document]:
+def create_chunks(library_name: str) -> List[Document]:
 
-    loader = DirectoryLoader(path=DIR_PATH)
+    loader = DirectoryLoader(path=DIR_PATH.format(library_name=library_name))
     text_splitter = RecursiveCharacterTextSplitter(
         separators=["\n\n"],
         chunk_size=CHUNK_SIZE,
@@ -32,13 +32,16 @@ def create_chunks() -> List[Document]:
 
     return chunks
     
-def create_index() -> Union[bool, Exception]:
+def create_index(library_name: str) -> Union[bool, Exception]:
 
     if not os.path.exists("./vectorstores"):
         os.mkdir("./vectorstores")
     
+    if os.path.exists(INDEX_PATH.format(library_name=library_name)):
+        return True
+    
     try:
-        chunks: List[Document] = create_chunks()
+        chunks: List[Document] = create_chunks(library_name=library_name)
 
         embedding = HuggingFaceBgeEmbeddings(
             model_name=EMBEDDING_MODEL,
@@ -48,17 +51,18 @@ def create_index() -> Union[bool, Exception]:
         )
 
         index = FAISS.from_documents(documents=chunks, embedding=embedding)
-        index.save_local(folder_path=INDEX_PATH)
+        index.save_local(folder_path=INDEX_PATH.format(library_name=library_name))
         return True
     except Exception as e:
         return e
 
 
 if __name__ == "__main__":
-
-    result = create_index()
-
-    if isinstance(result, bool):
-        print(f"Index successfully created at {INDEX_PATH}")
-    else:
-        print(f"Index not created due to {result}")
+    for dir in os.listdir("./data"):
+        path = os.path.join("./data", dir)
+        if os.path.isdir(path):
+            result = create_index(library_name=dir)
+            if isinstance(result, bool):
+                print(f"Index successfully created at {INDEX_PATH.format(library_name=dir)}")
+            else:
+                print(f"Index not created due to {result}")
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routes import chat
+from routes import chat, user
 
 app = FastAPI()
 
