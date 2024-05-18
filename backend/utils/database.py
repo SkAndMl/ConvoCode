037a16478b8181e8aa4205d2ebb687421ef98df5
Sathya Krishnan <86184014+SkAndMl@users.noreply.github.com>
2024-05-18 20:30:04 +0530

    Update database.py

--- backend/utils/database.py
+++ backend/utils/database.py
@@ -4,7 +4,7 @@ import os
 
 load_dotenv(find_dotenv())
 
-uri = "mongodb+srv://skandml:Sathya2406@cluster0.ynxtoer.mongodb.net/"
+uri = os.environ.get("MONGODB_URI")
 db = MongoClient(uri)["codetool"]
 
 collection_users = db["users"]
