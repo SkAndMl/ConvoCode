af6e73ac8c7e4d9bfacec6be00d9ce2798ef16cf
Sathya Krishnan <86184014+SkAndMl@users.noreply.github.com>
2024-05-18 19:06:04 +0530

    Add files via upload

---
+++ backend/utils/database.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+from dotenv import load_dotenv, find_dotenv
+import os
+
+load_dotenv(find_dotenv())
+
+uri = "mongodb+srv://skandml:Sathya2406@cluster0.ynxtoer.mongodb.net/"
+db = MongoClient(uri)["codetool"]
+
+collection_users = db["users"]
+collection_chats = db["chats"]
+collection_codes = db["codes"]
